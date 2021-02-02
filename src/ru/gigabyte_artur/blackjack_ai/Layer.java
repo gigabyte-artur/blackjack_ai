716bbb68c81a88a591748ac29d1da9c595040124
gigabyte-artur <gigabyte-artur@mail.ru>
2021-02-02 04:43:01 +0300

    (feat) Добавлен класс Layer

---
+++ src/ru/gigabyte_artur/blackjack_ai/Layer.java
@@ -0,0 +1,38 @@
+package ru.gigabyte_artur.blackjack_ai;
+
+import ru.gigabyte_artur.blackjack_ai.Neuron;
+
+import java.util.ArrayList;
+
+public class Layer
+{
+    private ArrayList<Neuron> Neurons = new ArrayList<>();
+
+    // Добавляет в текущий слой нейрон neuron_in.
+    public void AddNeuron(Neuron neuron_in)
+    {
+        Neurons.add(neuron_in);
+    }
+
+    // ДОбавляет в текущий слой count_in нейронов.
+    public void GenerateLayer(int count_in)
+    {
+        for (int i = 0; i < count_in; i++)
+        {
+            Neuron new_neuron = new Neuron();
+            this.AddNeuron(new_neuron);
+        }
+    }
+
+    // Выводит значения нейронов текущего слоя.
+    public void ShowLayer()
+    {
+        int curr_signal;
+        for (Neuron curr_neuron: Neurons)
+        {
+            curr_signal = curr_neuron.GetSignal();
+            System.out.print(curr_signal + " ");
+        }
+        System.out.println();
+    }
+}
--- src/ru/gigabyte_artur/blackjack_ai/Main.java
+++ src/ru/gigabyte_artur/blackjack_ai/Main.java
@@ -7,9 +7,12 @@ public class Main
 
     public static void main(String[] args)
     {
-        int rez_game;
-        GameBlackJack game1 = new GameBlackJack();
-        rez_game = game1.Play();
-        System.out.println(rez_game);
+//        int rez_game;
+//        GameBlackJack game1 = new GameBlackJack();
+//        rez_game = game1.Play();
+//        System.out.println(rez_game);
+        Layer layer1 = new Layer();
+        layer1.GenerateLayer(54);
+        layer1.ShowLayer();
     }
 }
