34cc940a13932adaeff1e408a947dc4866465c4f
gigabyte-artur <gigabyte-artur@mail.ru>
2021-03-02 03:13:53 +0300

    (feat) Добавлена сильная мутация при селекции

--- src/ru/gigabyte_artur/blackjack_ai/Selection.java
+++ src/ru/gigabyte_artur/blackjack_ai/Selection.java
@@ -9,11 +9,15 @@ public class Selection
     private int probablity_mutation;            // Вероятность мутации.
     private int mutated_player_number;          // Число мутирующих стратегий.
     private int mutated_out_number;             // Число выходных после мутирования стратегий.
+    private int probablity_hard_mutation;       // Вероятность сильной мутации.
+    private int hard_mutated_player_number;     // Число сильно мутирующих стратегий.
+    private int hard_mutated_out_number;        // Число выходных после сильного мутирования стратегий.
 
     // Осуществляет селекцию.
     public Selection()
     {
-        this.SetParameters(3, 5, 20, 60);
+        this.SetParameters(3, 5, 20, 65,
+                15, 20, 27);
     }
 
     // Выполняет селекцию поколения generation_in. Возвращает новое поколение.
@@ -44,16 +48,31 @@ public class Selection
             curr_neuro_net.Mutate(this.probablity_mutation);
             rez.AddPlayer(mutate_player);
         }
+        // Сильно мутирующие стратегии.
+        array_mutable = generation_in.GetTopPlayer(0, this.hard_mutated_player_number);
+        mutable_count = array_mutable.size();
+        for (int i = 0; i < this.hard_mutated_out_number; i++)
+        {
+            new_index = random.nextInt(mutable_count);
+            mutate_player = array_mutable.get(new_index);
+            curr_neuro_net = mutate_player.GetNeuroNet();
+            curr_neuro_net.Mutate(this.probablity_hard_mutation);
+            rez.AddPlayer(mutate_player);
+        }
         return rez;
     }
 
     // Устанавливает параметры текущей селекции, согласно переданным параметрам функции.
     public void SetParameters(int unсhangable_player_number_in, int probablitity_mutation_in, int mutated_player_number_in,
-        int mutated_out_number_in)
+        int mutated_out_number_in, int probablity_hard_mutation_in, int hard_mutated_player_number_in,
+        int hard_mutated_out_number_in)
     {
-        this.unсhangable_player_number = unсhangable_player_number_in;
-        this.probablity_mutation = probablitity_mutation_in;
-        this.mutated_player_number = mutated_player_number_in;
-        this.mutated_out_number = mutated_out_number_in;
+        this.unсhangable_player_number   = unсhangable_player_number_in;
+        this.probablity_mutation         = probablitity_mutation_in;
+        this.mutated_player_number       = mutated_player_number_in;
+        this.mutated_out_number          = mutated_out_number_in;
+        this.probablity_hard_mutation    = probablity_hard_mutation_in;
+        this.hard_mutated_player_number  = hard_mutated_player_number_in;
+        this.hard_mutated_out_number     = hard_mutated_out_number_in;
     }
 }
